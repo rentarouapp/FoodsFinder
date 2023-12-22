07e0f0de882bf158e2718a869ffe4b18dd71b5dc
uejo <rentarou1994@gmail.com>
2023-12-22 22:33:52 +0900

    Initial Commit

---
+++ FoodsFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodsFinder
+//
+//  Created by 上條蓮太朗 on 2023/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodsFinder/FoodsFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodsFinderApp.swift
+//  FoodsFinder
+//
+//  Created by 上條蓮太朗 on 2023/12/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodsFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
