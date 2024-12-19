581680b0214feb20fb9a67c30c3c01adc014b9f7
uejo <rentarou1994@gmail.com>
2024-12-19 15:43:38 +0900

    :art: お店のリクエストを叩くようにする

--- FoodsFinder/Features/ShopsFeature.swift
+++ FoodsFinder/Features/ShopsFeature.swift
@@ -12,21 +12,33 @@ import ComposableArchitecture
 struct ShopsFeature {
     @ObservableState
     struct State: Equatable {
-        var shops: [Shop] = []
+        var shops = Array<Shop>()
     }
     
     enum Action: Equatable {
-        case testA
-        case testB
+        case fetchShops(String) // キーワードをもらってお店一覧を取る
+        case setShops([Shop]) // 取れたお店をStateにセットする
+        case resetShops // お店一覧をリセット
     }
     
+    private let apiService = APIService()
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
-            case .testA:
+            case let .fetchShops(keyword):
+                return .run { send in
+                    let request = ShopInfoRequest(keyword: keyword)
+                    let shops = try await apiService.requestWithSwiftConcurrency(with: request).result?.shops ?? []
+                    await send(
+                        .setShops(shops)
+                    )
+                }
+                
+            case let .setShops(shops):
+                state.shops = shops
                 return .none
                 
-            case .testB:
+            case .resetShops:
                 return .none
             }
         }
