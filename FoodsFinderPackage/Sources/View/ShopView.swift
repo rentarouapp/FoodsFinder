fcdb41cfc72338d1e46844d89a4dd4148c381302
uejo <rentarou1994@gmail.com>
2025-01-11 15:20:36 +0900

    :bug: メインスレッドチェッカーのワーニングを消す

--- FoodsFinderPackage/Sources/View/ShopView.swift
+++ FoodsFinderPackage/Sources/View/ShopView.swift
@@ -29,43 +29,47 @@ public struct ShopView: View {
     let store: StoreOf<ShopsFeature>
     
     public var body: some View {
-        NavigationStack {
-            List(store.shops) { shop in
-                ZStack {
-                    NavigationLink(destination:
-                                    ShopDescriptionView(shop: shop)
-                        .toolbarRole(.editor) // Backは非表示にする
-                    ) {
-                        EmptyView()
+        WithPerceptionTracking {
+            // iOS17未満をターゲットにしているならWithPerceptionTrackingでラップする必要がある
+            // メインスレッドチェッカーが動いちゃう
+            NavigationStack {
+                List(store.shops) { shop in
+                    ZStack {
+                        NavigationLink(destination:
+                                        ShopDescriptionView(shop: shop)
+                            .toolbarRole(.editor) // Backは非表示にする
+                        ) {
+                            EmptyView()
+                        }
+                        // NavigationLinkの右の矢印を消す
+                        .opacity(0)
+                        ShopItemView(shop: shop)
                     }
-                    // NavigationLinkの右の矢印を消す
-                    .opacity(0)
-                    ShopItemView(shop: shop)
+                }
+                .navigationBarTitleDisplayMode(.large)
+                .navigationTitle("お店を探す")
+                .navigationBarHidden(false)
+            }
+            .listStyle(.plain)
+            .searchable(text: $searchText, prompt: "なんでも入力してね")
+            .onChange(of: searchText) { newValue in
+                // 検索バーの文字列が更新された
+                if newValue == "" {
+                    // クリアボタンがおされた
+                    self.focus = false
+                    self.store.send(.resetShops)
                 }
             }
-            .navigationBarTitleDisplayMode(.large)
-            .navigationTitle("お店を探す")
-            .navigationBarHidden(false)
-        }
-        .listStyle(.plain)
-        .searchable(text: $searchText, prompt: "なんでも入力してね")
-        .onChange(of: searchText) { newValue in
-            // 検索バーの文字列が更新された
-            if newValue == "" {
-                // クリアボタンがおされた
-                self.focus = false
-                self.store.send(.resetShops)
+            .onSubmit(of: .search) {
+                // 決定キー押された
+                if !self.searchText.isEmpty {
+                    self.focus = false
+                    self.store.send(.fetchShops(self.searchText))
+                }
             }
+            .focused(self.$focus)
+            //.PKHUD(isPresented: store.isFetching, HUDContent: .progress)
         }
-        .onSubmit(of: .search) {
-            // 決定キー押された
-            if !self.searchText.isEmpty {
-                self.focus = false
-                self.store.send(.fetchShops(self.searchText))
-            }
-        }
-        .focused(self.$focus)
-        //.PKHUD(isPresented: store.isFetching, HUDContent: .progress)
     }
 }
 
