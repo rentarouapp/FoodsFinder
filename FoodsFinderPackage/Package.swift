e08754296f7c4c51b7864ca449414e282912df8d
uejo <rentarou1994@gmail.com>
2025-01-11 15:42:20 +0900

    :recycle: CocoaNetworkingMonitorはバージョンで指定する、Package編集

--- FoodsFinderPackage/Package.swift
+++ FoodsFinderPackage/Package.swift
@@ -14,15 +14,15 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", exact: "1.16.1"),
-        .package(url: "https://github.com/rentarouapp/CocoaNetworkingMonitor.git", branch: "main"),
+        .package(url: "https://github.com/rentarouapp/CocoaNetworkingMonitor.git", .upToNextMajor(from: "0.1.0")),
     ],
     targets: [
         .target(name: "Core",
                 dependencies: [
                     "Entity",
                     "API",
+                    "CocoaNetworkingMonitor",
                     .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-                    .product(name: "CocoaNetworkingMonitor", package: "CocoaNetworkingMonitor"),
                 ]
                ),
         .target(name: "API",
