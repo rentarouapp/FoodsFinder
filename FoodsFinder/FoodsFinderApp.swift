30e6a8f67ec848146e7fa8c944fc9ed2ebd4bde1
uejo <rentarou1994@gmail.com>
2025-07-17 12:38:23 +0900

    :bug: FirebaseのTLS対応関連で起動時クラッシュする件の暫定対応を入れる

--- FoodsFinder/FoodsFinderApp.swift
+++ FoodsFinder/FoodsFinderApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import FirebaseCore
+import Network // Xcode26 × Firebase対応
 
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
@@ -20,6 +21,15 @@ class AppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct FoodsFinderApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+ 
+    init() {
+        if #available(iOS 26, *) {
+            // Xcode26 × Firebase対応
+            // iOS26, macOS26でTLS接続が1.2になったがFirebase側が対応していないので、クラッシュしないように暫定対応としてこうしておく
+            _ = nw_tls_create_options()
+        }
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
